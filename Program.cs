6fd5375900e0d54c96b50ee5db7e5245b117f5dc
TimurRM <tumyp.paxumob@gmail.com>
2022-06-23 23:59:50 +0500

    Added programm

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+// Console.Write("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int q = number * number;
+
+// Console.WriteLine(number + " -> " + q);
+
+
+// Console.Write("Input first number: ");
+// int number_1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int number_2 = Convert.ToInt32(Console.ReadLine());
+
+// if (number_1 == number_2*number_2)
+//     Console.WriteLine("a = " + number_1 + ", b =  " + number_2 + " ->  yes");
+// else 
+//     Console.WriteLine("a = " + number_1 + ", b =  " + number_2 + " ->  no");
+
+
+Console.Write("Input first number: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+int q = number_2 * number_2;
+
+if (number_1 == q)
+    Console.WriteLine("a = " + number_1 + ", b =  " + number_2 + " ->  yes");
+else 
+    Console.WriteLine("a = " + number_1 + ", b =  " + number_2 + " ->  no");
+
+
