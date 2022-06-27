abaf07d431078fa219aa76f8ee7acdb72771895c
Timur <tumyp.paxumob@gmail.com>
2022-06-27 22:06:30 +0500

    Added HW tasks

--- Program.cs
+++ Program.cs
@@ -17,16 +17,87 @@
 //     Console.WriteLine("a = " + number_1 + ", b =  " + number_2 + " ->  no");
 
 
-Console.Write("Input first number: ");
-int number_1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input second number: ");
-int number_2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input first number: ");
+// int number_1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int number_2 = Convert.ToInt32(Console.ReadLine());
 
-int q = number_2 * number_2;
+// int q = number_2 * number_2;
 
-if (number_1 == q)
-    Console.WriteLine("a = " + number_1 + ", b =  " + number_2 + " ->  yes");
-else 
-    Console.WriteLine("a = " + number_1 + ", b =  " + number_2 + " ->  no");
+// if (number_1 == q)
+//     Console.WriteLine("a = " + number_1 + ", b =  " + number_2 + " ->  yes");
+// else 
+//     Console.WriteLine("a = " + number_1 + ", b =  " + number_2 + " ->  no");
 
 
+// internal class Task_1
+// {
+//     private static void Main(string[] args)
+//     {
+//         Console.Write("Input first number: ");
+//         int num_1 = Convert.ToInt32(Console.ReadLine());
+//         Console.Write("Input second number: ");
+//         int num_2 = Convert.ToInt32(Console.ReadLine());
+
+
+//         if (num_1 > num_2)
+//             Console.WriteLine("a = " + num_1 + ", b =  " + num_2 + " -> max =  " + num_1 + "");
+//         else if (num_1 < num_2)
+//             Console.WriteLine("a = " + num_1 + ", b =  " + num_2 + " -> max =  " + num_2 + "");
+//         else
+//             Console.WriteLine("a = " + num_1 + ", b =  " + num_2 + " ->  a = b");
+//     }
+// }
+
+
+
+// internal class Task_2
+// {
+//     private static void Main(string[] args)
+//     {
+//         Console.Write("Input first number: ");
+//         int number_1 = Convert.ToInt32(Console.ReadLine());
+//         Console.Write("Input second number: ");
+//         int number_2 = Convert.ToInt32(Console.ReadLine());
+//         Console.Write("Input third number: ");
+//         int number_3 = Convert.ToInt32(Console.ReadLine());
+
+//         int max = 0;
+
+//         if (number_1 >= number_2 & number_1 >= number_3)
+//         max = number_1;
+    
+//             else if (number_2 >= number_1 & number_2 >= number_3)
+//         max = number_2;
+    
+//             else if (number_3 >= number_2 & number_3 >= number_1)
+//         max = number_3;
+//             Console.WriteLine(+ number_1 + "," + number_2 + "," + number_3 + " --> " + max);
+//     }
+// }
+
+
+
+// internal class Task_3
+// {
+//     private static void Main(string[] args)
+//     {
+//         Console.Write("Input number: ");
+//         int number = Convert.ToInt32(Console.ReadLine());
+        
+//         if (number % 2 == 0)
+//         Console.WriteLine(+ number + " --> " + "Yes");
+//         else 
+//         Console.WriteLine(+ number + " --> " + "No");
+
+//     }
+// }
+      
+
+// Console.Write("Input number: ")
+// int x = Convert.ToInt32(Console.ReadLine());
+
+
+// for  (x <= y; x ++)
+// if (x%2 ==0)
+// Console.WriteLine(x);
